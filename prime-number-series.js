d8c7127ac7527b1f00e1c7341a34b0e88238537e
marametro <marametro@gmail.com>
2022-11-28 12:12:18 +0700

    Updare reverse

--- prime-number-series.js
+++ prime-number-series.js
@@ -1,3 +1,20 @@
+/*
+    Secara garis besar, bilangan prima adalah bilangan yang hanya memiliki dua faktor, 
+    yaitu bilangan 1 dan bilangan itu sendiri.
+
+    Dengan kata lain, bilangan ini hanya bisa habis dibagi oleh angka 1 
+    dan dirinya sendiri serta tidak bisa dibagi oleh bilangan lainnya.
+
+    Agar lebih paham, kita bisa lihat bahwa angka 2 akan habis dibagi oleh angka 1 dan dirinya sendiri, yaitu 2, 
+    sedangkan angka 2 tidak bisa dibagi oleh angka atau bilangan lainnya.
+
+    Contoh lainnya adalah angka 3 yang hanya akan habis jika dibagi dengan angka 1 dan dirinya sendirinya.
+
+    Jadi, kesimpulannya, jika kamu ingin mengetahui bilangan apa saja yang termasuk ke dalam bil prima, 
+    maka kamu bisa mencoba dengan membagi bilangan tersebut, apakah hanya bisa memiliki dua faktor pembagi atau lebih.
+
+*/
+
 let n = 20;
 
 for (let i = 2; i < n; i++) {
---
+++ reverse-string.js
@@ -0,0 +1,18 @@
+
+/*
+    Reverse atau membalik 
+    Misal kita memiliki string caracter
+    Mara maka setelah di reverse/balik akan menjadi araM
+    contoh : 
+    mara = aram
+    apa  = apa
+    dia  = aid
+    makan malam = malam nakam
+
+
+*/
+
+
+let  kata = 'mara';
+
+console.log(kata.length)
