ff34b754d99268e4d602ff91c456d043401ca525
marametro <marametro@gmail.com>
2024-05-04 12:46:57 +0700

    add reverse string

--- reverse-string.js
+++ reverse-string.js
@@ -8,11 +8,62 @@
     apa  = apa
     dia  = aid
     makan malam = malam nakam
-
-
 */
 
 
-let  kata = 'mara';
+let  word = 'mara';
 
-console.log(kata.length)
+function reverse1(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+/*
+    In JavaScript, the split() method is used to divide
+    a string into an array of substrings based on a specified delimiter, 
+    and returns the new array. 
+*/
+console.log(word.split(''));
+
+/*
+    In JavaScript, to reverse elements, like the characters of a string or the items in an array, 
+    you can use different approaches depending on what exactly you're trying to reverse.
+*/
+console.log(word.split('').reverse(''));
+
+//Call function reverse1
+console.log(reverse1(word));
+
+
+function reverse2(str) {
+    
+    let result = '';
+    let length = str.length;
+
+    for (let i = length -1; i >= 0; i--) {
+        result +=str[i];
+    }
+    return result;
+}
+
+
+//Call function reverse2
+console.log(reverse2(word));
+
+
+function reverse3(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    console.log(character)
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+//Call function reverse3
+console.log(reverse3(word));
+  
